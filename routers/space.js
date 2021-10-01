7501c0f1fcc67675f97078978632cd9a81b33dac
Sara Ghaedi <saraghaedi75@gmail.com>
2021-10-01 15:23:14 +0200

    Add update space endpoint

--- routers/space.js
+++ routers/space.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = new Router();
 const Space = require("../models").space;
 const Story = require("../models").story;
+const authMiddleWare = require("../auth/middleware");
 
 // Get all Spaces
 
@@ -27,6 +28,30 @@ router.get("/:id", async (req, res, next) => {
     res.json(space);
   } catch (e) {
     console.log(e.message);
+  }
+});
+
+router.put("/:id", authMiddleWare, async (req, res, next) => {
+  try {
+    const spaceId = parseInt(req.params.id);
+    const spaceToBeUpdated = await Space.findByPk(spaceId);
+    if (!spaceToBeUpdated) {
+      return res.status(404).send({ message: "Space not found" });
+    }
+    const { title, description, backgroundColor, color } = req.body;
+    if (!title) {
+      return res.status(400).send({ message: "enter valid data" });
+    }
+    const updatedSpace = await spaceToBeUpdated.update({
+      title,
+      description,
+      backgroundColor,
+      color,
+    });
+    res.json(updatedSpace);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
   }
 });
 
